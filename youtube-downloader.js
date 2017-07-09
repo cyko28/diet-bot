6ae16e5b2c28633bd95521c9472ce33a14cf2005
Nick M <captfreeman@outlook.com>
2017-07-09 02:28:07 -0400

    Performing project cleanup. Making diet-bot.js the main JS file

--- youtube-downloader.js
+++
@@ -1,6 +0,0 @@
-var ytdl = require('youtube-dl');
-
-ytdl.exec('https://www.youtube.com/watch?v=a8c5wmeOL9o', ['-x', '--audio-format', 'mp3'], {}, function(err, output) {
-  if (err) throw err;
-  console.log(output.join('\n'));
-});
