3fb91d60ed574fcbc93b951e70e7e6205e0c20a4
Scott Robertson <scott.t.robertson@gmail.com>
2018-06-30 04:44:12 -0400

    added airhorn plugin

---
+++ src/plugins/airhorn/index.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+class Airhorn {
+    constructor() {
+        this.commands = ['a', 'airhorn'];
+    }
+    run(message, params = []) {
+        // Voice only works in guilds, if the message does not come from a guild,
+        // we ignore it
+        if (!message.guild) return;
+
+        // Only try to join the sender's voice channel if they are in one themselves
+        if (message.member.voiceChannel) {
+            message.member.voiceChannel
+                .join()
+                .then(connection => {
+                    const intent = connection.playStream(
+                        this.getAudio(params[0])
+                    );
+                    intent.on('end', () => connection.disconnect());
+                })
+                .catch(console.error);
+        } else {
+            message.reply('You need to join a voice channel first!');
+        }
+    }
+    getAudio(requestedName) {
+        let filepath = path.join(
+            __dirname,
+            'audio',
+            'airhorn-' + requestedName + '.mp3'
+        );
+        if (!fs.existsSync(filepath)) {
+            filepath = path.join(__dirname, 'audio', 'airhorn.mp3');
+        }
+        return fs.createReadStream(filepath);
+    }
+}
+
+module.exports = Airhorn;
