a5a88326206b8bc1798afbfc94d4acae575cfcd2
Scott Robertson <scott.t.robertson@gmail.com>
2018-06-30 15:08:53 -0400

    new plugins

---
+++ src/plugins/rave/index.js
@@ -0,0 +1,46 @@
+const fs = require('fs-extra');
+const path = require('path');
+
+class Rave {
+    constructor() {
+        this.commands = ['r', 'rave'];
+        this.soundFiles = [];
+    }
+    init() {
+        this.findAudioFiles();
+    }
+    run(message, params = []) {
+        // Voice only works in guilds, if the message does not come from a guild,
+        // we ignore it
+        if (!message.guild) return;
+
+        // Only try to join the sender's voice channel if they are in one themselves
+        if (message.member.voiceChannel) {
+            message.member.voiceChannel
+                .join()
+                .then(connection => {
+                    const intent = connection.playStream(this.playAudio());
+                    intent.on('end', () => connection.disconnect());
+                })
+                .catch(console.error);
+        } else {
+            message.reply('You need to join a voice channel first!');
+        }
+    }
+    findAudioFiles() {
+        fs.readdir(path.join(__dirname, 'audio'), (err, files) => {
+            this.soundFiles = files;
+        });
+    }
+    playAudio() {
+        const randomFile = this.soundFiles[
+            this.randomInt(this.soundFiles.length)
+        ];
+        return fs.createReadStream(path.join(__dirname, 'audio', randomFile));
+    }
+    randomInt(n) {
+        return Math.floor(Math.random() * n);
+    }
+}
+
+module.exports = Rave;
