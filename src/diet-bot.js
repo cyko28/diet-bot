176348fcf79aff6cbea8f02f857d54a1b3bb4197
Scott Robertson <scott.t.robertson@gmail.com>
2020-07-20 13:00:39 -0400

    added ascii intro

--- src/diet-bot.js
+++ src/diet-bot.js
@@ -22,8 +22,117 @@ const pluginNames = [
 const commandQueue = new CommandQueue(bot, { pluginNames });
 
 bot.on('ready', () => {
-    console.log(`Logged in as ${bot.user.tag}!`);
-    commandQueue.init();
+    const speedGap = 25;
+    setTimeout(() => {
+        console.log(`                  ,,.─╖▓▓█▓╣░   ░▒▓▓████▄▄╦,`);
+    }, 1 * speedGap);
+    setTimeout(() => {
+        console.log(`               ╫▄,░╓q▓╬╢╢▓▀╙╙ⁿⁿ░╨╝▒░▒▒▓███▀╣`);
+    }, 2 * speedGap);
+    setTimeout(() => {
+        console.log(`               ▄█▓▓▓▓▓╢╢▒▒▒▒▒▒▒▒▒ßßß╢╢▒▒▒@▓▓`);
+    }, 3 * speedGap);
+    setTimeout(() => {
+        console.log(`             ,█▓▓████▓▓▓▓▓╣╢╢╣▒▒▒▒▒▒▓▓╣╢╢╣▓▓▓,`);
+    }, 4 * speedGap);
+    setTimeout(() => {
+        console.log(`            ╒▓▓▓▓▓███▓▓▓▓▓╣╢╢▒╢╢╢╣╣▒▒▓▓▓▓▓▓▓╣╢▓`);
+    }, 5 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▓▓▓▓▓▓▓▓▓╣╢╢╢╣▒▒▒▒▒▒▒▒▒▓▓▓▓▓▓▒╫▓`);
+    }, 6 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▓▓▓▓▓╣╢╣▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓▓▓▌╫▓`);
+    }, 7 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▓▓, ▀▓▓╣▒▒▒▒▒▒▒▒╖╓░ ▒▒▒▒▓▓▓▓▓▓╫▓`);
+    }, 8 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▓▓▓""╕ ╟▒╜          ▒▒▒▒▒▓▓▓▓▓▓▓`);
+    }, 9 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▓▓▀▌, ^╟▒  ╓╖╖╖╖╖╖╖▒▒▒▒▒▒▓▓▓▓▌▓▓`);
+    }, 10 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▓▓▓ⁿ¬, ▓▒╖└╣▒▒▒▒▒▒▒▒▒▒▒▒▒▒▓▓▓╣▓▓`);
+    }, 11 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▓╓R▓&┐ ╣▒           ▒▒▒▒▒╢▓██╣▓▓`);
+    }, 12 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▌▓█- ^▓╘▒,╓╖╖╖╖╖╖╖⌐ ▒▒▒▒▒╢▓██╢▓▓`);
+    }, 13 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▓╙█L▐▓æ╔▒╜          ▒▒▒▒▒╢▓█▌╢▓▓`);
+    }, 14 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▓▓▄▄▓╣╣╣╢  ,,,,,,  ]▒▒▒▒▒╢██╣╢▓▓`);
+    }, 15 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▓▓▓▓▓╣╣╣[ ▒▒▒▒▒▒▒▒▒ ▒▒▒▒▒▒█▌╣╣▓▓`);
+    }, 16 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▓▓▓▓▓╣╣╣╢   ^^^^^   ▒▒▒▒▒██▒╣╢▓▓`);
+    }, 17 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▓▓▓▓▓╣╣╣▒╢╖       ,▒▒▒▒▒▒▓█╢╣╣▓▓`);
+    }, 18 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▓▓▓▓▓"╙╙╙╙╙╙╙╙""╙╙╙╙▒▒▒▒▓▓▌╢╣╢▓▓`);
+    }, 19 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▓▓▓▓▓▓▓▌               ▒▒▒▓▓▓▒╣╣╣▓▓`);
+    }, 20 * speedGap);
+    setTimeout(() => {
+        console.log(`            ╟▓▓▓▓▓▓▓▓▓╣╢▒▒▒▒▒▒╥,╙▒▒▒▒▒▓▓▓╣╢╣╣▓▓`);
+    }, 21 * speedGap);
+    setTimeout(() => {
+        console.log(`            ╟▓▓▓▓▓▓ ,g╣▒▒▒▒▒▒▒▒▒▒ ╙▒▒▒▓▓▓╢╢╢╢▓▓`);
+    }, 22 * speedGap);
+    setTimeout(() => {
+        console.log(`            ╟▓▓▓▓▓▌j╣╣╣▒▒▒▒▒▒▒▒▒▒▒ ▒▒▓▓▓▓╢╢╢╢╣▓`);
+    }, 23 * speedGap);
+    setTimeout(() => {
+        console.log(`            ╟▓▓▓▓▓▌ ▓╣▒▒▒▒▒▒▒▒▒▒▒^ ▒▒▓▓▓▓╣╢╢╢╢▓`);
+    }, 24 * speedGap);
+    setTimeout(() => {
+        console.log(`            ╟▓▓▓▓▓▓╖  ╙╙╝╝╝╨╨╜╙   ╓▒▒▓▓▓▓▒╢╢╢╢▓`);
+    }, 25 * speedGap);
+    setTimeout(() => {
+        console.log(`            ▐▓▓▓▓▓▓▓▓,          ,╥▒▒▒▓▓▓▓▌╢╢╣╢Γ`);
+    }, 26 * speedGap);
+    setTimeout(() => {
+        console.log(`             ╙▓▓▓▓▓▓▓╢╢╣@@╗╗╥╥╢▒▒▒▒▒▒█▓▓▓▓▒▒▓"`);
+    }, 27 * speedGap);
+    setTimeout(() => {
+        console.log(`                "▓▓▓▓╣╢╣▒▒▒▒▒▒▒▒▒▒▒▒▒▓█▀▀▀^`);
+    }, 28 * speedGap);
+    setTimeout(() => {
+        console.log(`                   '^╜╜░░░^^^'^^^    ^`);
+    }, 29 * speedGap);
+    setTimeout(() => {
+        console.log(
+            `\x1b[31m%s\x1b[0m`,
+            `
+
+\`7MM"""Yb.     db           mm       \`7MM"""Yp,           mm
+  MM    \`Yb.                MM         MM    Yb           MM
+  MM     \`Mb \`7MM  .gP"Ya mmMMmm       MM    dP  ,pW"Wq.mmMMmm
+  MM      MM   MM ,M'   Yb  MM         MM"""bg. 6W'   \`Wb MM
+  MM     ,MP   MM 8M""""""  MM         MM    \`Y 8M     M8 MM
+  MM    ,dP'   MM YM.    ,  MM         MM    ,9 YA.   ,A9 MM
+.JMMmmmdP'   .JMML.\`Mbmmd'  \`Mbmo    .JMMmmmd9   \`Ybmd9'  \`Mbmo
+`
+        );
+    }, 30 * speedGap);
+
+    setTimeout(() => {
+        console.log(`Logged in as ${bot.user.tag}!`);
+    }, 31 * speedGap);
+
+    setTimeout(() => {
+        commandQueue.init();
+    }, 31 * speedGap);
 });
 
 bot.on('message', (msg) => {
