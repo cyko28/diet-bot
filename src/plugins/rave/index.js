e0736c9f9a6f11411babcceaf42f6355bb72a4e7
unknown <neg_zero@hotmail.com>
2021-03-03 21:31:41 -0500

    Adding new airhorn. Adding random chance of dietbot dance

--- src/plugins/rave/index.js
+++ src/plugins/rave/index.js
@@ -1,5 +1,6 @@
 const fs = require('fs-extra');
 const path = require('path');
+const Discord = require('discord.js');
 
 class Rave {
     constructor(bot, commandQueue) {
@@ -21,7 +22,7 @@ class Rave {
         // Only try to join the sender's voice channel if they are in one themselves
         if (message.member.voice.channel) {
             const connection = await message.member.voice.channel.join();
-
+            this.sendDanceEmbed();
             const dispatcher = connection.play(this.playAudio());
             dispatcher.on('finish', () => {
                 connection.disconnect();
@@ -36,6 +37,48 @@ class Rave {
     playAudio() {
         const random = Math.floor(Math.random() * this.audioFilePaths.length);
         return path.join(__dirname, 'audio', this.audioFilePaths[random]);
+    }
+    sendDanceEmbed() {
+        const showDance = Math.random() < 0.09 ? true : false;
+
+        if (showDance) {
+            const channel = this.getDietBotChannel();
+            let danceEmbed = new Discord.MessageEmbed().attachFiles(
+                'https://thumbs.gfycat.com/CourteousRemarkableArrowcrab-max-1mb.gif'
+            );
+            channel.send(danceEmbed);
+
+            // Delete the message after 5 seconds
+            setTimeout(
+                async function () {
+                    channel.messages
+                        .fetch({
+                            limit: 10,
+                        })
+                        .then((messages) => {
+                            const dietBotId = '279879398323257346';
+                            const amountToDelete = 1;
+                            messages = messages
+                                .filter((m) => m.author.id === dietBotId)
+                                .array()
+                                .slice(0, amountToDelete);
+                            channel
+                                .bulkDelete(messages)
+                                .catch((error) => console.log(error.stack));
+                        });
+                }.bind(this),
+                7000
+            );
+        }
+    }
+
+    getDietBotChannel() {
+        const dietClanID = '169703392749289472';
+        const dietClanGuild = this.bot.guilds.cache
+            .array()
+            .find((el) => el.id === dietClanID);
+        const dietBotChannelID = '462447510456107029';
+        return dietClanGuild.channels.cache.get(dietBotChannelID);
     }
 }
 
